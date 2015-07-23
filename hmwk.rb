d7262997d49f90cb56ff0f9e3a59739b65f12fa8
kamutu <wangechililian@gmail.com>
2015-07-23 16:31:28 +0300

    first commit

---
+++ hmwk.rb
@@ -0,0 +1,22 @@
+irb(main):001:0> math=90
+=> 90
+irb(main):002:0> eng=85
+=> 85
+irb(main):003:0> chem=88
+=> 88
+irb(main):004:0> bio=72
+=> 72
+irb(main):005:0> biz=86
+=> 86
+irb(main):006:0> sum=math+ eng +chem +bio +biz
+=> 421
+irb(main):007:0> if sum >=400
+irb(main):008:1> print 'a'
+irb(main):009:1> elsif sum < 400 && sum >=380
+irb(main):010:1> print 'b+'
+irb(main):011:1> elsif sum < 379 && sum >=350
+irb(main):012:1> print 'c+'
+irb(main):013:1> elsif sum < 349 && sum >=320
+irb(main):014:1> print 'd'
+irb(main):015:1> end
+a=> nil
